682c799889ea648d28281c3fe576a79062ae5d0b
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-05-12 23:25:29 +0530

    update resume.routes

--- routes/resume.routes.js
+++ routes/resume.routes.js
@@ -508,14 +508,14 @@ ${certificates.map(c => `
 
     /* ===== PUPPETEER ===== */
 
-    browser = await puppeteer.launch({
+browser = await puppeteer.launch({
 
   executablePath:
-  process.env.PUPPETEER_EXECUTABLE_PATH,
+    process.env.PUPPETEER_EXECUTABLE_PATH,
 
-  headless:"new",
+  headless: "new",
 
-  args:[
+  args: [
     "--no-sandbox",
     "--disable-setuid-sandbox",
     "--disable-dev-shm-usage",
